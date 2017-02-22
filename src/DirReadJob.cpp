2cbf3321bf37e67591b8738a0b4c77a96fd345a6
Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
2017-02-22 20:28:53 +0100

    Slow down reading for debugging

--- src/DirReadJob.cpp
+++ src/DirReadJob.cpp
@@ -630,6 +630,8 @@ DirReadJobQueue::~DirReadJobQueue()
 
 void DirReadJobQueue::enqueue( DirReadJob * job )
 {
+    static int count = 0;
+
     if ( job )
     {
 	_queue.append( job );
@@ -639,8 +641,11 @@ void DirReadJobQueue::enqueue( DirReadJob * job )
 	{
 	    // logDebug() << "First job queued" << endl;
 	    emit startingReading();
-	    _timer.start( 0 );
+            _timer.start( 0 );
 	}
+
+        if ( ++count == 500 )
+            _timer.start( 200 );
     }
 }
 
