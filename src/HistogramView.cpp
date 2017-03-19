ee34eec84d3e5afb5044441eaca8d57fa2a49e5a
Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
2017-03-19 15:42:48 +0100

    Suppress quantiles if no data

--- src/HistogramView.cpp
+++ src/HistogramView.cpp
@@ -442,52 +442,66 @@ void HistogramView::addXStartEndLabels()
 
 void HistogramView::addQuartileText()
 {
-    QString q1Text = tr( "Q1: %1" ).arg( formatSize( percentile( 25 ) ) );
-    QString q3Text = tr( "Q3: %1" ).arg( formatSize( percentile( 75 ) ) );
-    QString medianText = tr( "Median: %1" ).arg( formatSize( percentile( 50 ) ) );
-
-    QGraphicsSimpleTextItem * q1Item     = scene()->addSimpleText( q1Text );
-    QGraphicsSimpleTextItem * q3Item     = scene()->addSimpleText( q3Text );
-    QGraphicsSimpleTextItem * medianItem = scene()->addSimpleText( medianText );
-
-    q1Item->setBrush( _quartilePen.color() );
-    q3Item->setBrush( _quartilePen.color() );
-    medianItem->setBrush( _medianPen.color() );
-
-    QFont font( medianItem->font() );
-    font.setBold( true );
-
-    medianItem->setFont( font );
-    q1Item->setFont( font );
-    q3Item->setFont( font );
-
     qreal textBorder  = 10.0;
     qreal textSpacing = 30.0;
 
-    qreal y = -_histogramHeight - _topBorder - textBorder;
-    y -= medianItem->boundingRect().height();
-
-    qreal q1Width     = q1Item->boundingRect().width();
-    qreal q3Width     = q3Item->boundingRect().width();
-    qreal medianWidth = medianItem->boundingRect().width();
-
     qreal x = 0;
-    q1Item->setPos( x, y );      x += q1Width     + textSpacing;
-    medianItem->setPos( x, y );  x += medianWidth + textSpacing;
-    q3Item->setPos( x, y );      x += q3Width     + textSpacing;
+    qreal y = -_histogramHeight - _topBorder - textBorder;
+    qreal n = bucketsTotalSum();
+
+    if ( n > 0 ) // Only useful if there are any data at all
+    {
+        QString q1Text = tr( "Q1: %1" ).arg( formatSize( percentile( 25 ) ) );
+        QString q3Text = tr( "Q3: %1" ).arg( formatSize( percentile( 75 ) ) );
+        QString medianText = tr( "Median: %1" ).arg( formatSize( percentile( 50 ) ) );
+
+        QGraphicsSimpleTextItem * q1Item     = scene()->addSimpleText( q1Text );
+        QGraphicsSimpleTextItem * q3Item     = scene()->addSimpleText( q3Text );
+        QGraphicsSimpleTextItem * medianItem = scene()->addSimpleText( medianText );
+
+        q1Item->setBrush( _quartilePen.color() );
+        q3Item->setBrush( _quartilePen.color() );
+        medianItem->setBrush( _medianPen.color() );
+
+        QFont font( medianItem->font() );
+        font.setBold( true );
+
+        medianItem->setFont( font );
+        q1Item->setFont( font );
+        q3Item->setFont( font );
+
+        y -= medianItem->boundingRect().height();
+
+        qreal q1Width     = q1Item->boundingRect().width();
+        qreal q3Width     = q3Item->boundingRect().width();
+        qreal medianWidth = medianItem->boundingRect().width();
+
+        q1Item->setPos( x, y );      x += q1Width     + textSpacing;
+        medianItem->setPos( x, y );  x += medianWidth + textSpacing;
+        q3Item->setPos( x, y );      x += q3Width     + textSpacing;
+    }
 
 
-    QString nText;
+    //
+    // Add text for the total number of files
+    //
+
+    QString nText = tr( "Files (n): %1" ).arg( n );
+    QGraphicsSimpleTextItem * nTextItem = scene()->addSimpleText( nText );
+
+    QFont font( nTextItem->font() );
+    font.setBold( true );
+    nTextItem->setFont( font );
+
     QFontMetrics metrics( font );
     QChar sigma( UNICODE_MATH_SIGMA );
 
     if ( metrics.inFont( sigma ) )
-        nText = QString( "%1n: %2" ).arg( sigma ).arg( bucketsTotalSum() );
-    else
-        nText = tr( "Files (n): %1" ).arg( bucketsTotalSum() );
+        nTextItem->setText( QString( "%1n: %2" ).arg( sigma ).arg( n ) );
 
-    QGraphicsSimpleTextItem * nTextItem = scene()->addSimpleText( nText );
-    nTextItem->setFont( font );
+    if ( n == 0 )
+        y -= nTextItem->boundingRect().height();
+
     nTextItem->setPos( x, y );
 }
 
