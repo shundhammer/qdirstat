d7264e75dacbd2ed62ae041a3a978f5a63f33938
Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
2018-10-06 17:09:41 +0200

    Caching for PkgQuery

--- src/PkgManager.cpp
+++ src/PkgManager.cpp
@@ -15,7 +15,9 @@
 #include "Exception.h"
 
 
-#define COMMAND_TIMEOUT_SEC 5
+#define COMMAND_TIMEOUT_SEC	5
+#define CACHE_SIZE		500
+#define CACHE_COST		1
 
 using namespace QDirStat;
 
@@ -37,6 +39,8 @@ PkgQuery * PkgQuery::instance()
 
 PkgQuery::PkgQuery()
 {
+    _cache.setMaxCost( CACHE_SIZE );
+
     tryAdd( new DpkgPkgManager() );
     tryAdd( new RpmPkgManager()	 );
 }
@@ -54,8 +58,12 @@ void PkgQuery::tryAdd( PkgManager * pkgManager )
 
     if ( pkgManager->isActiveOnSystem() )
     {
-        logInfo() << "Found package manager " << pkgManager->name() << endl;
+	logInfo() << "Found package manager " << pkgManager->name() << endl;
 	_pkgManagers << pkgManager;
+    }
+    else
+    {
+	delete pkgManager;
     }
 }
 
@@ -68,33 +76,60 @@ QString PkgQuery::owningPkg( const QString & path )
 
 QString PkgQuery::getOwningPackage( const QString & path )
 {
-    foreach ( PkgManager * pkgManager, _pkgManagers )
-    {
-	QString pkg = pkgManager->owningPkg( path );
+    QString pkg = "";
+    QString foundBy;
+    bool haveResult = false;
 
-	if ( ! pkg.isEmpty() )
-	{
-	    logDebug() << pkgManager->name()
-                       << ": Package " << pkg << " owns " << path << endl;
-	    return pkg;
-	}
+    if ( _cache.contains( path ) )
+    {
+	haveResult = true;
+	foundBy	   = "Cache";
+	pkg	   = *( _cache[ path ] );
     }
 
-    return "";
+
+    if ( ! haveResult )
+    {
+	foreach ( PkgManager * pkgManager, _pkgManagers )
+	{
+	    pkg	    = pkgManager->owningPkg( path );
+
+	    if ( ! pkg.isEmpty() )
+	    {
+		haveResult = true;
+		foundBy	   = pkgManager->name();
+		break;
+	    }
+	}
+
+	if ( foundBy.isEmpty() )
+	    foundBy = "all";
+
+	// Insert package name (even if empty) into the cache
+	_cache.insert( path, new QString( pkg ), CACHE_COST );
+    }
+
+    if ( pkg.isEmpty() )
+	logDebug() << ( ! foundBy.isEmpty() ? ( foundBy + ": " ) : "" )
+		   << "No package owns " << path << endl;
+    else
+	logDebug() << foundBy << ": Package " << pkg << " owns " << path << endl;
+
+    return pkg;
 }
 
 
 
 bool PkgManager::tryRunCommand( const QString & commandLine,
-                                const QRegExp & expectedResult ) const
+				const QRegExp & expectedResult ) const
 {
     int exitCode = -1;
     QString output = runCommand( commandLine, &exitCode );
 
     if ( exitCode != 0 )
     {
-        logDebug() << "Exit code: " << exitCode << " command line: \"" << commandLine << "\"" << endl;
-        return false;
+	logDebug() << "Exit code: " << exitCode << " command line: \"" << commandLine << "\"" << endl;
+	return false;
     }
 
     bool expected = expectedResult.exactMatch( output );
@@ -105,10 +140,10 @@ bool PkgManager::tryRunCommand( const QString & commandLine,
 
 
 QString PkgManager::runCommand( const QString & commandLine,
-                                int *           exitCode_ret ) const
+				int *		exitCode_ret ) const
 {
     if ( exitCode_ret )
-        *exitCode_ret = -1;
+	*exitCode_ret = -1;
 
     QStringList args = commandLine.split( QRegExp( "\\s+" ) );
 
@@ -124,12 +159,12 @@ QString PkgManager::runCommand( const QString & commandLine,
 }
 
 
-QString PkgManager::runCommand( const QString &     command,
-                                const QStringList & args,
-                                int *               exitCode_ret ) const
+QString PkgManager::runCommand( const QString &	    command,
+				const QStringList & args,
+				int *		    exitCode_ret ) const
 {
     if ( exitCode_ret )
-        *exitCode_ret = -1;
+	*exitCode_ret = -1;
 
     if ( access( command.toUtf8(), X_OK ) != 0 )
     {
@@ -145,28 +180,28 @@ QString PkgManager::runCommand( const QString &     command,
     process.setArguments( args );
     process.setProcessEnvironment( env );
     process.setProcessChannelMode( QProcess::MergedChannels ); // combine stdout and stderr
-    // logDebug() << "Running \"" << command << "\" args: " << args << endl;
+    logDebug() << "Running \"" << command << "\" args: " << args << endl;
     process.start();
     bool success = process.waitForFinished( COMMAND_TIMEOUT_SEC * 1000 );
     QString output = QString::fromUtf8( process.readAll() );
 
     if ( success )
     {
-        if ( process.exitStatus() == QProcess::NormalExit )
-        {
-            if ( exitCode_ret )
-                *exitCode_ret = process.exitCode();
-        }
-        else
-        {
-            logError() << "Command crashed: \"" << command << "\" args: " << args << endl;
-            output = "ERROR: Command crashed\n\n" + output;
-        }
+	if ( process.exitStatus() == QProcess::NormalExit )
+	{
+	    if ( exitCode_ret )
+		*exitCode_ret = process.exitCode();
+	}
+	else
+	{
+	    logError() << "Command crashed: \"" << command << "\" args: " << args << endl;
+	    output = "ERROR: Command crashed\n\n" + output;
+	}
     }
     else
     {
-        logError() << "Timeout or crash: \"" << command << "\" args: " << args << endl;
-        output = "ERROR: Timeout or crash\n\n" + output;
+	logError() << "Timeout or crash: \"" << command << "\" args: " << args << endl;
+	output = "ERROR: Timeout or crash\n\n" + output;
     }
 
     // logDebug() << "Output: \n" << output << endl;
@@ -189,7 +224,7 @@ QString DpkgPkgManager::owningPkg( const QString & path )
     QString output = runCommand( "/usr/bin/dpkg", QStringList() << "-S" << path, &exitCode );
 
     if ( exitCode != 0 || output.contains( "no path found matching pattern" ) )
-        return "";
+	return "";
 
     QString pkg = output.section( ":", 0, 0 );
     // logDebug() << path << " is owned by pkg " << pkg << endl;
@@ -210,11 +245,11 @@ QString RpmPkgManager::owningPkg( const QString & path )
 {
     int exitCode = -1;
     QString output = runCommand( "/usr/bin/rpm",
-                                 QStringList() << "-qf" << "--queryformat" << "%{NAME}" << path,
-                                 &exitCode );
+				 QStringList() << "-qf" << "--queryformat" << "%{NAME}" << path,
+				 &exitCode );
 
     if ( exitCode != 0 || output.contains( "not owned by any package" ) )
-        return "";
+	return "";
 
     QString pkg = output;
     // logDebug() << path << " is owned by pkg " << pkg << endl;
--- src/PkgManager.h
+++ src/PkgManager.h
@@ -11,6 +11,7 @@
 #define PkgManager_h
 
 #include <QString>
+#include <QCache>
 
 
 namespace QDirStat
@@ -25,45 +26,46 @@ namespace QDirStat
     {
     public:
 
-        /**
-         * Return the owning package of a file or directory with full path
-         * 'path' or an empty string if it is not owned by any package.
-         **/
-        static QString owningPkg( const QString & path );
+	/**
+	 * Return the owning package of a file or directory with full path
+	 * 'path' or an empty string if it is not owned by any package.
+	 **/
+	static QString owningPkg( const QString & path );
 
-        /**
-         * Return the singleton instance of this class.
-         **/
-        static PkgQuery * instance();
+	/**
+	 * Return the singleton instance of this class.
+	 **/
+	static PkgQuery * instance();
 
     protected:
 
-        /**
-         * Constructor. For internal use only; use the static methods instead.
-         **/
-        PkgQuery();
+	/**
+	 * Constructor. For internal use only; use the static methods instead.
+	 **/
+	PkgQuery();
 
-        /**
-         * Destructor.
-         **/
-        virtual ~PkgQuery();
+	/**
+	 * Destructor.
+	 **/
+	virtual ~PkgQuery();
 
-        /**
-         * Return the owning package of a file or directory with full path
-         * 'path' or an empty string if it is not owned by any package.
-         **/
-        QString getOwningPackage( const QString & path );
+	/**
+	 * Return the owning package of a file or directory with full path
+	 * 'path' or an empty string if it is not owned by any package.
+	 **/
+	QString getOwningPackage( const QString & path );
 
-        /**
-         * Try to add a package manager to the internal list.
-         **/
-        void tryAdd( PkgManager * pkgManager );
+	/**
+	 * Try to add a package manager to the internal list.
+	 **/
+	void tryAdd( PkgManager * pkgManager );
 
 
-        // Data members
+	// Data members
 
-        static PkgQuery *     _instance;
-        QList <PkgManager *>  _pkgManagers;
+	static PkgQuery *	 _instance;
+	QList <PkgManager *>	 _pkgManagers;
+	QCache<QString, QString> _cache;
 
     }; // class PkgQuery
 
@@ -75,93 +77,93 @@ namespace QDirStat
     {
     public:
 
-        /**
-         * Constructor.
-         **/
-        PkgManager() {}
+	/**
+	 * Constructor.
+	 **/
+	PkgManager() {}
 
-        /**
-         * Destructor.
-         **/
-        virtual ~PkgManager() {}
+	/**
+	 * Destructor.
+	 **/
+	virtual ~PkgManager() {}
 
-        /**
-         * Return the name of this package manager.
-         *
-         * Derived classes are required to implement this.
-         **/
-        virtual QString name() const = 0;
+	/**
+	 * Return the name of this package manager.
+	 *
+	 * Derived classes are required to implement this.
+	 **/
+	virtual QString name() const = 0;
 
-        /**
-         * Check if this package manager is active on the currently running
-         * system.
-         *
-         * Derived classes are required to implement this.
-         *
-         * Remember that a system might support installing 'foreign' package
-         * managers; for example, on Debian / Ubuntu you can also install the
-         * 'rpm' package. It is strongly advised to do a more sophisticated
-         * test than just checking if a certain executable (like /usr/bin/dpkg
-         * or /usr/bin/rpm) exists.
-         *
-         * The PkgQuery class will only execute this once at its startup phase,
-         * so this information does not need to be cached.
-         **/
-        virtual bool isActiveOnSystem() = 0;
+	/**
+	 * Check if this package manager is active on the currently running
+	 * system.
+	 *
+	 * Derived classes are required to implement this.
+	 *
+	 * Remember that a system might support installing 'foreign' package
+	 * managers; for example, on Debian / Ubuntu you can also install the
+	 * 'rpm' package. It is strongly advised to do a more sophisticated
+	 * test than just checking if a certain executable (like /usr/bin/dpkg
+	 * or /usr/bin/rpm) exists.
+	 *
+	 * The PkgQuery class will only execute this once at its startup phase,
+	 * so this information does not need to be cached.
+	 **/
+	virtual bool isActiveOnSystem() = 0;
 
-        /**
-         * Return the owning package of a file or directory with full path
-         * 'path' or an empty string if it is not owned by any package.
-         *
-         * Derived classes are required to implement this.
-         **/
-        virtual QString owningPkg( const QString & path ) = 0;
+	/**
+	 * Return the owning package of a file or directory with full path
+	 * 'path' or an empty string if it is not owned by any package.
+	 *
+	 * Derived classes are required to implement this.
+	 **/
+	virtual QString owningPkg( const QString & path ) = 0;
 
     protected:
 
-        /**
-         * Try running a command and compare it against an expected result.
-         * Return 'true' if ok, 'false' if not.
-         **/
-        bool tryRunCommand( const QString & commandLine,
-                            const QRegExp & expectedResult ) const;
+	/**
+	 * Try running a command and compare it against an expected result.
+	 * Return 'true' if ok, 'false' if not.
+	 **/
+	bool tryRunCommand( const QString & commandLine,
+			    const QRegExp & expectedResult ) const;
 
-        /**
-         * Run a command line and return its output. If exitCode_ret is
-         * non-null, return the command's exit code in exitCode_ret.
-         *
-         * Notice 1: This uses a very basic command line parser; it simply
-         * splits the command up whereever whitespace might occur. If any of
-         * the arguments (no matter how sophisticated they might be quoted)
-         * possibly contains any whitespace, this is unsafe; in that case, use
-         * the overloaded version instead that accepts a QStringList as
-         * arguments.
-         *
-         * Notice 2: This does not start a shell with that command, it runs the
-         * command directly, so only binaries can be executed, no shell scripts
-         * or scripts of other interpreted languages. If that is desired, wrap
-         * the command into "/bin/sh -c".
-         **/
-        QString runCommand( const QString & commandLine,
-                            int *           exitCode_ret ) const;
+	/**
+	 * Run a command line and return its output. If exitCode_ret is
+	 * non-null, return the command's exit code in exitCode_ret.
+	 *
+	 * Notice 1: This uses a very basic command line parser; it simply
+	 * splits the command up whereever whitespace might occur. If any of
+	 * the arguments (no matter how sophisticated they might be quoted)
+	 * possibly contains any whitespace, this is unsafe; in that case, use
+	 * the overloaded version instead that accepts a QStringList as
+	 * arguments.
+	 *
+	 * Notice 2: This does not start a shell with that command, it runs the
+	 * command directly, so only binaries can be executed, no shell scripts
+	 * or scripts of other interpreted languages. If that is desired, wrap
+	 * the command into "/bin/sh -c".
+	 **/
+	QString runCommand( const QString & commandLine,
+			    int *	    exitCode_ret ) const;
 
-        /**
-         * Run a command with arguments 'args' and return its output. If
-         * exitCode_ret is non-null, return the command's exit code in
-         * exitCode_ret.
-         *
-         * Use this version to avoid any side effects due to command line
-         * parsing.
-         *
-         * Notice: This does not start a shell with that command, it runs the
-         * command directly, so only binaries can be executed, no shell scripts
-         * or scripts of other interpreted languages. If that is desired, use
-         * "/bin/sh" as the command, "-c" as the first argument and the command
-         * line to be executed as the second. Beware of shell quoting quirks!
-         **/
-        QString runCommand( const QString &     command,
-                            const QStringList & args,
-                            int *               exitCode_ret ) const;
+	/**
+	 * Run a command with arguments 'args' and return its output. If
+	 * exitCode_ret is non-null, return the command's exit code in
+	 * exitCode_ret.
+	 *
+	 * Use this version to avoid any side effects due to command line
+	 * parsing.
+	 *
+	 * Notice: This does not start a shell with that command, it runs the
+	 * command directly, so only binaries can be executed, no shell scripts
+	 * or scripts of other interpreted languages. If that is desired, use
+	 * "/bin/sh" as the command, "-c" as the first argument and the command
+	 * line to be executed as the second. Beware of shell quoting quirks!
+	 **/
+	QString runCommand( const QString &	command,
+			    const QStringList & args,
+			    int *		exitCode_ret ) const;
 
     }; // class PkgManager
 
@@ -177,34 +179,34 @@ namespace QDirStat
     {
     public:
 
-        DpkgPkgManager() {}
-        virtual ~DpkgPkgManager() {}
+	DpkgPkgManager() {}
+	virtual ~DpkgPkgManager() {}
 
-        /**
-         * Return the name of this package manager.
-         *
-         * Implemented from PkgManager.
-         **/
-        virtual QString name() const { return "dpkg"; }
+	/**
+	 * Return the name of this package manager.
+	 *
+	 * Implemented from PkgManager.
+	 **/
+	virtual QString name() const { return "dpkg"; }
 
-        /**
-         * Check if dpkg is active on the currently running system.
-         *
-         * Implemented from PkgManager.
-         **/
-        virtual bool isActiveOnSystem() Q_DECL_OVERRIDE;
+	/**
+	 * Check if dpkg is active on the currently running system.
+	 *
+	 * Implemented from PkgManager.
+	 **/
+	virtual bool isActiveOnSystem() Q_DECL_OVERRIDE;
 
-        /**
-         * Return the owning package of a file or directory with full path
-         * 'path' or an empty string if it is not owned by any package.
-         *
-         * Implemented from PkgManager.
-         *
-         * This basically executes this command:
-         *
-         *   /usr/bin/dpkg -S ${path}
-         **/
-        virtual QString owningPkg( const QString & path ) Q_DECL_OVERRIDE;
+	/**
+	 * Return the owning package of a file or directory with full path
+	 * 'path' or an empty string if it is not owned by any package.
+	 *
+	 * Implemented from PkgManager.
+	 *
+	 * This basically executes this command:
+	 *
+	 *   /usr/bin/dpkg -S ${path}
+	 **/
+	virtual QString owningPkg( const QString & path ) Q_DECL_OVERRIDE;
 
     }; // class DpkgPkgManager
 
@@ -213,34 +215,34 @@ namespace QDirStat
     {
     public:
 
-        RpmPkgManager() {}
-        virtual ~RpmPkgManager() {}
+	RpmPkgManager() {}
+	virtual ~RpmPkgManager() {}
 
-        /**
-         * Return the name of this package manager.
-         *
-         * Implemented from PkgManager.
-         **/
-        virtual QString name() const { return "rpm"; }
+	/**
+	 * Return the name of this package manager.
+	 *
+	 * Implemented from PkgManager.
+	 **/
+	virtual QString name() const { return "rpm"; }
 
-        /**
-         * Check if RPM is active on the currently running system.
-         *
-         * Implemented from PkgManager.
-         **/
-        virtual bool isActiveOnSystem() Q_DECL_OVERRIDE;
+	/**
+	 * Check if RPM is active on the currently running system.
+	 *
+	 * Implemented from PkgManager.
+	 **/
+	virtual bool isActiveOnSystem() Q_DECL_OVERRIDE;
 
-        /**
-         * Return the owning package of a file or directory with full path
-         * 'path' or an empty string if it is not owned by any package.
-         *
-         * Implemented from PkgManager.
-         *
-         * This basically executes this command:
-         *
-         *   /usr/bin/rpm -qf ${path}
-         **/
-        virtual QString owningPkg( const QString & path ) Q_DECL_OVERRIDE;
+	/**
+	 * Return the owning package of a file or directory with full path
+	 * 'path' or an empty string if it is not owned by any package.
+	 *
+	 * Implemented from PkgManager.
+	 *
+	 * This basically executes this command:
+	 *
+	 *   /usr/bin/rpm -qf ${path}
+	 **/
+	virtual QString owningPkg( const QString & path ) Q_DECL_OVERRIDE;
 
     }; // class RpmPkgManager
 
