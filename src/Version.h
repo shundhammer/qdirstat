4cb9ccee245412aafaadc51e86a7c883fab4dd40
Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
2020-07-26 15:13:13 +0200

    Post-release version bump

--- src/Version.h
+++ src/Version.h
@@ -9,7 +9,7 @@
 #ifndef Version_h
 #define Version_h
 
-#define QDIRSTAT_VERSION "1.7"
+#define QDIRSTAT_VERSION "1.7.01-git"
 
 #define RELEASE_VERSION  "1.7"
 #define RELEASE_BASE_URL "https://github.com/shundhammer/qdirstat/releases/tag/"
