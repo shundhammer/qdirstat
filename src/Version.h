691df791f63aaaa513e72a77437a82027eb925c7
Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
2024-01-14 16:25:28 +0100

    Post-release version bump for further development

--- src/Version.h
+++ src/Version.h
@@ -9,8 +9,8 @@
 #ifndef Version_h
 #define Version_h
 
-#define QDIRSTAT_VERSION "1.9"
-#define MENUBAR_VERSION "V1.9 "
+#define QDIRSTAT_VERSION "1.9.01-git"  // add ".01-git" after a release
+#define MENUBAR_VERSION  "V1.9+ "      // add "+ "      after a release
 
 #define RELEASE_VERSION  "1.9"
 #define RELEASE_BASE_URL "https://github.com/shundhammer/qdirstat/releases/tag/"
