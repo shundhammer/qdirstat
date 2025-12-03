cf53a9a93452396d48d9f975eb2298892c8e8bdc
Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
2025-12-03 15:15:14 +0100

    Version bump to 1.95

--- src/Version.h
+++ src/Version.h
@@ -9,10 +9,10 @@
 #ifndef Version_h
 #define Version_h
 
-#define QDIRSTAT_VERSION "1.9.01-git"  // add ".01-git" after a release
-#define MENUBAR_VERSION  "V1.9+ "      // add "+ "      after a release
+#define QDIRSTAT_VERSION "1.95.01-git"  // add ".01-git" after a release
+#define MENUBAR_VERSION  "V1.95+ "      // add "+ "      after a release
 
-#define RELEASE_VERSION  "1.9"
+#define RELEASE_VERSION  "2.0"
 #define RELEASE_BASE_URL "https://github.com/shundhammer/qdirstat/releases/tag/"
 #define RELEASE_URL      RELEASE_BASE_URL RELEASE_VERSION
 
